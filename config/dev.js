f2078889c48ee6692b67abd5e6fa8473a0dcd70c
Charles <chibuzc@yahoo.co.uk>
2017-10-05 12:43:59 +0100

    auth flow

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '425279532715-q49o73k9gggv7c5j6g07ud5o142f29lo.apps.googleusercontent.com',
+    googleClientSecret: 't-08XzvFHniusxGf0F4n0bz7',
+    mongoURL: 'mongodb://chibuzc:5cx6-Iwf@ds147044.mlab.com:47044/react_practice',
+    cookieKey: 'rftyhjcukyswgjcauhbdjciqhnckhdjcqa'
+  }
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const passport = require('passport')
+require('./models/user')
+require('./services/passport')
+
+
+const keys = require('./config/keys')
+const cookieSession = require("cookie-session")
+
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.json({test : "okay"})
-})
+app.use(cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys: [keys.cookieKey]
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
+
+mongoose.connect(keys.mongoURL)
+
 
 const PORT = process.env.PORT
 app.listen(PORT || 5000)
