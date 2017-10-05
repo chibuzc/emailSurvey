270794a06baff3b991939f3fab72ece488ce840b
Charles <chibuzc@yahoo.co.uk>
2017-10-05 17:06:38 +0100

    auth flow

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,6 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURL: process.env.MONGO_URL,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    proxy: true
   }
